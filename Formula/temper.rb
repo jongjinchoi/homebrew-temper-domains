efa042a58b3f604d8b1a67ed89a0d684d3f230e8
Jongjin Choi <me@jongjinchoi.com>
2026-07-22 13:16:44 +0900

    temper 0.3.4

--- Formula/temper.rb
+++ Formula/temper.rb
@@ -1,25 +1,25 @@
 class Temper < Formula
   desc "Never leave your terminal to find a domain"
   homepage "https://github.com/jongjinchoi/temper-domains"
-  version "0.3.3"
+  version "0.3.4"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/jongjinchoi/temper-domains/releases/download/v0.3.3/temper-bun-darwin-arm64.tar.gz"
-      sha256 "da66028754e4195b402eea1d2918a62ece50bd86b333298598d04b1bf9ad4bed"
+      url "https://github.com/jongjinchoi/temper-domains/releases/download/v0.3.4/temper-bun-darwin-arm64.tar.gz"
+      sha256 "063b4f555d95538b88ce3df92452f74fb781de5c6e2c12887064c45f0aaa582a"
     else
-      url "https://github.com/jongjinchoi/temper-domains/releases/download/v0.3.3/temper-bun-darwin-x64.tar.gz"
-      sha256 "0ddfe22a5aa40c418893cc41d70617168ab075af1a64f4b66dcd57bddaf7e872"
+      url "https://github.com/jongjinchoi/temper-domains/releases/download/v0.3.4/temper-bun-darwin-x64.tar.gz"
+      sha256 "ea3121aaf620cdbb970a2db1db0a695a2b5121ef00a44d5e19f7f6940fc6f404"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/jongjinchoi/temper-domains/releases/download/v0.3.3/temper-bun-linux-arm64.tar.gz"
-      sha256 "5df9d08ba425a3548f475d65a79c2ac3de2abd491ededb258ee387d8f843b7da"
+      url "https://github.com/jongjinchoi/temper-domains/releases/download/v0.3.4/temper-bun-linux-arm64.tar.gz"
+      sha256 "fd2a5c60535ab1aba4e569060fe9690262385b8356bd5d90e1c8bfcaa400efa7"
     else
-      url "https://github.com/jongjinchoi/temper-domains/releases/download/v0.3.3/temper-bun-linux-x64.tar.gz"
-      sha256 "5441d8f4a08b4c0e06741a3b49184e9cc1d50eef89e7cae9947883e78e67e1a4"
+      url "https://github.com/jongjinchoi/temper-domains/releases/download/v0.3.4/temper-bun-linux-x64.tar.gz"
+      sha256 "cf94013bdafc0f364f39ca81f00fb496a03fa0e24bfa81edd5d98a4b00644765"
     end
   end
 
